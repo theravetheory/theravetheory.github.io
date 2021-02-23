b47c09d0ffceefa1586dbdc86a9acd2fbc88addb
evansavage <esav678@gmail.com>
2021-02-23 11:27:27 -0500

    Three.js logo addition

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './trt_logo.png';
+import logosquaresmall from './trtsquaresmall.png';
+import icon from './trt_favicon.png';
 import './Fonts.css';
 import './App.scss';
 import './Scroll.scss';
 import AudioPlayer from './AudioPlayer.js';
-import HomeBkgd from './VantaBkgd.js'
+import HomeBkgd from './VantaBkgd.js';
+import ThreeCube from './THREECube.js';
 
 
 function App() {
@@ -11,13 +14,14 @@ function App() {
   return (
     <div className="app-wrapper">
       <HomeBkgd />
+
       <div className="nav-wrapper">
         <nav className="navbar">
           {// <div className="icon-container">
           // </div>
           }
           <div className="icon-container">
-            <img className="App-logo" src={logo} alt="The rave theory logo" />
+            <ThreeCube imagetexture={logo}/>
           </div>
           <AudioPlayer streamurl={stream_url}/>
         </nav>
@@ -26,7 +30,9 @@ function App() {
         <header className="App-header">
         </header>
       </div>
+
     </div>
+
   );
 }
 
---
+++ src/THREECube.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import ReactDOM from "react-dom";
+import * as THREE from "three";
+import $ from 'jquery';
+
+class ThreeCube extends Component {
+  componentDidMount() {
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera( 90, 1);
+    var renderer = new THREE.WebGLRenderer({antialias:true, alpha: true});
+    renderer.setSize(50, 50);
+    // document.body.appendChild( renderer.domElement );
+    this.mount.appendChild(renderer.domElement);
+    var geometry = new THREE.BoxGeometry( 10, 10, 10 );
+    var material = new THREE.MeshBasicMaterial();
+    var cube = new THREE.Mesh( geometry, material );
+    var tex = new THREE.TextureLoader().load(
+      this.props.imagetexture,
+      texture => {
+        //Update Texture
+        cube.material.map = texture;
+        cube.material.needsUpdate = true;
+        cube.scale.set(1.0, texture.image.height / texture.image.width, 1.0);
+      },
+      xhr => {
+        //Download Progress
+        console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+      },
+      error => {
+        //Error CallBack
+        console.log("An error happened" + error);
+      }
+    );
+    scene.add( cube );
+    camera.position.z = 10;
+    var animate = function () {
+      requestAnimationFrame( animate );
+      // cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      renderer.render( scene, camera );
+    };
+    animate();
+  }
+
+  render() {
+    return (
+      <div className="three-cube" ref={(mount) => {this.mount = mount;}}></div>
+    )
+  }
+}
+
+export default ThreeCube;
