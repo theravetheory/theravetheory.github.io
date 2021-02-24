f5059f32b1efde8d76655385cf3146fadc2a93da
evansavage <esav678@gmail.com>
2021-02-24 00:58:35 -0500

    Created Realm app and anonymous login

--- src/App.js
+++ src/App.js
@@ -1,16 +1,67 @@
 import logo_small from './logos/trt_logo_small.png';
 // import logosquaresmall from './logos/trtsquaresmall.png';
 // import icon from './logos/trt_favicon.png';
+import React from 'react';
 import './Fonts.css';
 import './scss/App.scss';
 import './scss/Scroll.scss';
 import AudioPlayer from './components/AudioPlayer.js';
 import HomeBkgd from './components/VantaBkgd.js';
 import ThreeCube from './components/THREECube.js';
+import * as Realm from "realm-web";
+import $ from 'jquery';
+
+const REALM_APP_ID = "theravetheoryradio-rducr"; // e.g. myapp-abcde
+const app: Realm.App = new Realm.App({ id: REALM_APP_ID });
+
+// Create a component that displays the given user's details
+function UserDetail({ user, username, setUser}) {
+  const logoutAnonymous = async () => {
+    await app.currentUser.logOut();
+    setUser(false);
+  }
+  return (
+    <>
+    <div>
+      <h1>Logged in with anonymous id: {user.id} and username {username}</h1>
+    </div>
+    <button onClick={logoutAnonymous}>Log Out</button>
+    </>
+  );
+}
+
+// Create a component that lets an anonymous user log in
+function Login({ setUser, setUserName }) {
+  const loginAnonymous = async () => {
+    console.log($('#login-name input').val());
+    setUserName($('#login-name input').val());
+    const user = await app.logIn(Realm.Credentials.anonymous());
+    setUser(user);
+
+
+  };
+  return (
+    <>
+    <label id="login-name"> Name: <input type="text"/></label>
+    <button onClick={loginAnonymous}>Log In</button>
+    </>
+  );
+  // return (
+  //     <form >
+  //       <label>
+  //         Name:
+  //         <input type="text"  />
+  //       </label>
+  //       <input type="submit" value="Login" onClick={loginAnonymous}/>
+  //     </form>
+  //   );
+}
 
 
 function App() {
   const stream_url = "https://theravetheory.evan-savage.com:8000";
+  const [user, setUser] = React.useState(app.currentUser);
+  const [username, setUserName] = React.useState();
   return (
     <div className="app-wrapper">
       <HomeBkgd />
@@ -25,6 +76,7 @@ function App() {
       </div>
       <div>
         <header className="App-header">
+          {user ? <UserDetail user={user} username={username} setUser={setUser}/> : <Login setUser={setUser} setUserName={setUserName}/>}
         </header>
       </div>
 
