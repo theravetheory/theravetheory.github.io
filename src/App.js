2e31bfaaa3881b7ae5b7906e3fa708b1932d1860
harper.shapiro <harper.shapiro@gmail.com>
2021-05-11 10:07:34 -0500

    refactored winamp module, added mp3

--- src/App.js
+++ src/App.js
@@ -1,33 +1,40 @@
-import logo_small from './logos/trt_logo.png';
+import logo_small from "./logos/trt_logo.png";
 // import logosquaresmall from './logos/trtsquaresmall.png';
 // import icon from './logos/trt_favicon.png';
 // import React from 'react';
-import './Fonts.css';
-import './scss/App.scss';
-import './scss/Scroll.scss';
-import AudioPlayer from './components/AudioPlayer.js';
-import HomeBkgd from './components/VantaBkgd.js';
-import ThreeCube from './components/THREECube.js';
-import VRScene from './components/VRScene.js';
-import { Entity, Scene } from 'aframe-react';
-import React, {useState, useEffect} from 'react';
-import Webamp from 'webamp';
+import "./Fonts.css";
+import "./scss/App.scss";
+import "./scss/Scroll.scss";
+import AudioPlayer from "./components/AudioPlayer.js";
+import HomeBkgd from "./components/VantaBkgd.js";
+import ThreeCube from "./components/THREECube.js";
+import VRScene from "./components/VRScene.js";
+import Winamp from "./components/Winamp/Winamp.js";
+import { Entity, Scene } from "aframe-react";
+import React, { useState, useEffect } from "react";
+import Webamp from "webamp";
 
 import * as Realm from "realm-web";
-import $ from 'jquery';
+import $ from "jquery";
+
+import testMp3 from "./music/shadaff_oomoo.mp3";
 
 const REALM_APP_ID = "theravetheoryradio-rducr"; // e.g. myapp-abcde
 const app: Realm.App = new Realm.App({ id: REALM_APP_ID });
+const testTrack = { url: testMp3 };
 
 // Create a component that displays the given user's details
-function UserDetail({ user, username, setUser}) {
+function UserDetail({ user, username, setUser }) {
   const logoutAnonymous = async () => {
     await app.currentUser.logOut();
     setUser(false);
-  }
+  };
   return (
     <div className="login-wrapper">
-      <h1>Logged in with anonymous id: <span>{user.id}</span> and username <span>{username}</span></h1>
+      <h1>
+        Logged in with anonymous id: <span>{user.id}</span> and username{" "}
+        <span>{username}</span>
+      </h1>
       <button onClick={logoutAnonymous}>Log Out</button>
     </div>
   );
@@ -36,19 +43,21 @@ function UserDetail({ user, username, setUser}) {
 // Create a component that lets an anonymous user log in
 function Login({ setUser, setUserName }) {
   const loginAnonymous = async () => {
-    console.log($('#login-name input').val());
-    setUserName($('#login-name input').val());
+    console.log($("#login-name input").val());
+    setUserName($("#login-name input").val());
     const user = await app.logIn(Realm.Credentials.anonymous());
     setUser(user);
   };
   return (
     <div className="login-wrapper">
-      <label id="login-name"> Name: <input type="text"/></label>
+      <label id="login-name">
+        {" "}
+        Name: <input type="text" />
+      </label>
       <button onClick={loginAnonymous}>Log In</button>
     </div>
   );
 }
-
 
 function App() {
   const stream_url = "https://theravetheory.evan-savage.com:8000";
@@ -59,13 +68,15 @@ function App() {
 
   useEffect(() => {
     callBackendAPI()
-    .then(res => setDataState(res.express))
-    .catch(err => console.log(err));
+      .then((res) => setDataState(res.express))
+      .catch((err) => console.log(err));
 
     if (webampDivRef == null) {
       return;
     }
-    const webamp = new Webamp({});
+    const webamp = new Webamp({
+      initialTracks: [testTrack]
+    });
     webamp.renderWhenReady(webampDivRef);
 
     return () => {
@@ -73,44 +84,45 @@ function App() {
     };
   }, [webampDivRef]);
 
-    // Update the document title using the browser API
+  // Update the document title using the browser API
 
-  
-
-  const callBackendAPI = async() => {
-    const response = await fetch('/express_backend');
+  const callBackendAPI = async () => {
+    const response = await fetch("/express_backend");
     const body = await response.json();
 
     if (response.status !== 200) {
-      throw Error(body.message)
+      throw Error(body.message);
     }
     return body;
-  }
+  };
 
   // const webamp = new Webamp({});
   // webamp.renderWhenReady(document.getElementById('winamp-container'));
 
   return (
     <div className="app-wrapper">
-      <div id="webamp-container" ref={setWebampDivRef} />;
+      {/* <div id="webamp-container" ref={setWebampDivRef} />; */}
+      <Winamp />
       <VRScene />
       <h1>{dataState}</h1>
       <div className="nav-wrapper">
         <nav className="navbar">
           <div className="icon-container">
-            <ThreeCube imagetexture={logo_small}/>
+            <ThreeCube imagetexture={logo_small} />
           </div>
-          <AudioPlayer streamurl={stream_url}/>
+          <AudioPlayer streamurl={stream_url} />
         </nav>
       </div>
       <div>
         <header className="App-header">
-          {user ? <UserDetail user={user} username={username} setUser={setUser}/> : <Login setUser={setUser} setUserName={setUserName}/>}
+          {user ? (
+            <UserDetail user={user} username={username} setUser={setUser} />
+          ) : (
+            <Login setUser={setUser} setUserName={setUserName} />
+          )}
         </header>
       </div>
-
     </div>
-
   );
 }
 
---
+++ src/components/Winamp/config.js
@@ -0,0 +1,5 @@
+import shadaffOomoo from "../../music/shadaff_oomoo.mp3";
+
+export const initialTracks = [
+    { url: shadaffOomoo }
+];
