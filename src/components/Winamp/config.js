f0367a2a5f3b5a2bdd7db6508a7908f1f6cbecb9
harper.shapiro <harper.shapiro@gmail.com>
2021-05-11 10:15:41 -0500

    fixed stream crash bug

--- src/components/Winamp/config.js
+++ src/components/Winamp/config.js
@@ -1,5 +1,5 @@
-import shadaffOomoo from "../../music/shadaff_oomoo.mp3";
+// import shadaffOomoo from "../../music/shadaff_oomoo.mp3";
 
 export const initialTracks = [
-    { url: shadaffOomoo }
+    { url: "https://raw.githubusercontent.com/theravetheory/theravetheory.github.io/winamp/src/music/shadaff_oomoo.mp3" }
 ];
