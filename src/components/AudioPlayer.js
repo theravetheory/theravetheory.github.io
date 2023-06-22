785eef0d52d321902a3c0e7255f14a5f15626bee
evansavage <esav678@gmail.com>
2023-06-21 21:29:01 -0400

    prettified and plugged into new icecast server

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -1,87 +1,84 @@
-import React, {Component} from 'react';
-import $ from 'jquery';
-// import ReactDOM from 'react-dom';
-import PlayPause from './PlayPause.js';
-// import useAudioPlayer from './useAudioPlayer.js';
+import React, { useEffect, useState } from "react";
+import $ from "jquery";
+import PlayPause from "./PlayPause.js";
 
-
-function AudioEmbed(name, description, genre) {
-  // const { playing, setPlaying } = useAudioPlayer();
-  const isplaying = false;
+const AudioEmbed = ({ serverState }) => {
+  const { name, description, genre } = serverState;
+  const isPlaying = false;
   return (
     <>
-    <audio controls id="stream-player">
-      <source src="https://theravetheory.evan-savage.com:8000/stream" type="audio/mpeg"></source>
-    </audio>
-    <div className="scroll-left">
-      <p>Now playing: {name} - {description} [{genre}]</p>
-    </div>
-    <div className="player-controls">
-      <PlayPause isplaying={isplaying}/>
-    </div>
+      <audio controls id="stream-player">
+        <source
+          src="http://54.163.26.105:8000/stream"
+          type="audio/mpeg"
+        ></source>
+      </audio>
+      <div className="scroll-left">
+        <p>
+          Now playing: {name} - {description} [{genre}]
+        </p>
+      </div>
+      <div className="player-controls">
+        <PlayPause isplaying={isPlaying} />
+      </div>
     </>
   );
-}
+};
 
-export default class AudioPlayer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      status: null,
-      error: null,
-      serverName: null,
-      serverDescription: null,
-      serverGenre: null,
-    };
-  }
-  componentDidMount() {
-    fetch(this.props.streamurl + "/stream")
-    .then((response) => {
-      this.setState({
-        status: response.status,
+const AudioPlayer = ({ streamURL }) => {
+  const [serverState, setServerState] = useState({
+    serverName: null,
+    serverDescription: null,
+    serverGenre: null,
+    status: null,
+  });
+
+  useEffect(() => {
+    debugger;
+    fetch(streamURL + "/stream")
+      .then((response) => {
+        debugger;
+        setServerState({
+          ...serverState,
+          status: response.status,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        console.log("stream is down, son.");
       });
-      console.log('stream', this.state);
-    });
     $.getJSON({
-      url: this.props.streamurl + "/status-json.xsl",
+      url: streamURL + "/status-json.xsl",
       success: (data, status, xhr) => {
-        console.log(xhr)
+        console.log(xhr);
         if (data.icestats.source) {
-          this.setState({
+          setServerState({
+            ...serverState,
             serverName: data.icestats.source.server_name,
             serverDescription: data.icestats.source.server_description,
             serverGenre: data.icestats.source.genre,
           });
         }
       },
-      error: function(data) {
-        console.log('there was an error');
-      }
+      error: function (data) {
+        console.log("there was an error");
+      },
     });
-  }
+  }, []);
 
-  render() {
-    const status = this.state.status;
+  return serverState.status === 200 ? (
+    <AudioEmbed serverState={serverState} />
+  ) : serverState.status === 404 ? (
+    <div className="scroll-left">
+      <p>
+        Donate to <a href="mailto:theravetheory0@gmail.com">theravetheory</a>
+      </p>
+    </div>
+  ) : (
+    <div className="scroll-left">
+      <p> Loading... </p>
+    </div>
+  );
+};
 
-    // console.log(element);
-    if (status === 200) {
-      return (
-        AudioEmbed(this.state.serverName, this.state.serverDescription, this.state.serverGenre)
-      )
-    } else if (status === 404) {
-      return (
-        <div className="scroll-left">
-          <p>Donate to <a href="mailto:theravetheory0@gmail.com">theravetheory</a></p>
-        </div>
-      )
-    } else {
-      return (
-        <div className="scroll-left">
-          <p> Loading... </p>
-        </div>
-
-      );
-    }
-  }
-
-}
+export default AudioPlayer;
